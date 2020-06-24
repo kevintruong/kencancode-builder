9e3580fa458a06c6a82af62308a8189bc9642d3a
Vu Truong <vu.truong@veriksystems.com>
2020-06-24 05:16:38 -0400

    update project

--- tf_builder.sh
+++ tf_builder.sh
@@ -3,6 +3,8 @@
 
 command=$1
 tf_build_prefix=$2
+args=$3
+hostname=$4
 
 export TF_PROJECT_PREFIX=${tf_build_prefix}
 mkdir -p ${TF_PROJECT_PREFIX}
@@ -11,5 +13,6 @@ envsubst < ./temp/backend_gcs_temp.tf > backend_conf_${TF_PROJECT_PREFIX}.tf;
 echo run "terraform init -backend-config=./backend_conf_${TF_PROJECT_PREFIX}.tf"
 terraform init -backend-config=./backend_conf_${TF_PROJECT_PREFIX}.tf ;
 
-terraform plan -var prefix=${TF_PROJECT_PREFIX}
-terraform ${command} -var prefix=${TF_PROJECT_PREFIX}
+terraform plan -var prefix=${TF_PROJECT_PREFIX} ${args}
+terraform ${command} -var prefix=${TF_PROJECT_PREFIX} ${args}
+sudo sed -i "/$hostname/c $(echo `terraform output ip` ${hostname})" /etc/hosts
